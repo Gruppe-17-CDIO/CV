26495e46043cba05c0a1fd47713af536373f62b4
Logstor <a.rottger_rydahl@live.dk>
2020-03-05 09:17:00 +0100

    Initial commit

---
+++ OpenImaj/src/main/java/FindColour.java
@@ -0,0 +1,65 @@
+import org.openimaj.image.MBFImage;
+
+import java.awt.*;
+
+public class FindColour
+{
+	
+	private float threshold;
+	private float ratio;
+	
+	public FindColour(float threshold, float ratio)
+	{
+		this.ratio = ratio;
+		this.threshold = threshold;
+	}
+	
+	public boolean find(Colour colourToFind, Point start, Point end, MBFImage image)
+	{
+		switch (colourToFind)
+		{
+			case RED:
+				return findRed(start, end, image);
+				
+			case GREEN:
+				return findGreen(start, end, image);
+				
+			case BLUE:
+				return false;
+		}
+		
+		return false;
+	}
+	
+	private boolean findRed(Point start, Point end, MBFImage image)
+	{
+		// Find areal af kvadrant
+		int area = (end.x - start.x) * (end.y - start.y);
+		
+		int redCount = 0;
+		for (int y=start.y; y < end.y; y++)
+		{
+				for (int x=start.x; x < end.x; x++)
+				{
+					float[][] band0 = image.getBand(0).pixels;
+					float[][] band1 = image.getBand(1).pixels;
+					float[][] band2 = image.getBand(2).pixels;
+					
+					float accum = band0[y][x] + band1[y][x] + band2[y][x];
+					
+					if (accum/band0[y][x] < threshold)
+						redCount++;
+				}
+		}
+		float ratio = (float)redCount / (float)area;
+		if (redCount > 0)
+			System.out.println(String.format("area: %s\tredCount: %s\tRatio: %s\n", area, redCount, ratio));
+		
+		return ratio > this.ratio;
+	}
+	
+	private boolean findGreen(Point start, Point end, MBFImage image)
+	{
+		return false;
+	}
+}
---
+++ OpenImaj/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.Image;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.pixel.Pixel;
+
+import javax.swing.text.Utilities;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
+		final float THRESHOLD = 2.6f;
+		final int SCALE = 12;
+		
+		MBFImage image = ImageUtilities.readMBF(new File("BaneMedBolde0.jpg"));
+		MBFImage clone = image.clone();
+		MBFImage raw = new MBFImage(image.getWidth()/SCALE, image.getHeight()/SCALE);
+		
+		for (int height=0; height < raw.getHeight(); height++)
+		{
+			for (int width=0; width < raw.getWidth(); width++)
+			{
+				FindColour finder 	= new FindColour(THRESHOLD, 0.2f);
+				Point start 		= new Point(SCALE*width, SCALE*height);
+				Point end 			= new Point(SCALE*width+(SCALE-1), SCALE*height+(SCALE-1));
+				
+				if (finder.find(Colour.RED, start, end, image))
+					raw.getBand(0).pixels[height][width] = 1f;
+				else if (finder.find(Colour.GREEN, start, end, image))
+					raw.getBand(1).pixels[height][width] = 1f;
+				else if (finder.find(Colour.BLUE, start, end, image))
+					raw.getBand(2).pixels[height][width] = 1f;
+			}
+		}
+		
+		DisplayUtilities.display(raw).setSize(2400, 1440);
+	}
+	
+	public static float redRatio(final int width, final int height, MBFImage image)
+	{
+		float[][] band0 = image.getBand(0).pixels;
+		float[][] band1 = image.getBand(1).pixels;
+		float[][] band2 = image.getBand(2).pixels;
+		
+		float accum = band0[height][width] + band1[height][width] + band2[height][width];
+		
+		return accum/band0[height][width];
+	}
+}
